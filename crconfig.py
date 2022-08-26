a84a3af8fc79244f4d252c77d362a6d4b57b017d
vovanchubenko <112155197+vovanchubenko@users.noreply.github.com>
2022-08-26 09:23:01 +0300

    Add files via upload

---
+++ crconfig.py
@@ -0,0 +1,17 @@
+TOKEN='5796227133:AAGjNB_wi6chVwa5HMLEGz48YXaEDIQHWLI'
+MONGO_TOKEN='mongodb+srv://bot1:bot1@cluster0.tybln6n.mongodb.net/?retryWrites=true&w=majority'
+emoji={"robot":u'\U0001F916','rocket':u'\U0001F680',"feet":u'\U0001F463','star':u'\U0001F4AB','house':u'\U0001F3E1','sofa':u'\U0001F6CB',
+       'net':u'\U0001F310','clock':u'\U0001F552','phone':u'\U0000260E',"time":u'\U0001F551',"quest":u'\U00002753',"save":u'\U0001F4BE',
+       "cam":u'\U0001F4F7',"rev":u'\U0001F4DD',"back":u'\U000021A9',"mai":u'\U0001F4E9',"ab":u'\U0001F530',"lo":u'\U0001F512',
+       "unlo":u'\U0001F513',"yep":u'\U00002705',"book":u'\U0001F4D6',"left":u'\U00002B05',"right":u'\U000027A1',"tochka":u'\U00002716',
+       "maps":u'\U0001F5FA',"red":u'\U0001F534',"green":u'\U0001F7E2','redc':u'\U000026D4', 'moon':u'\U0001F319', 'sun':u'\U00002600',
+       'city':u'\U0001F3D9', 'evn':u'\U0001F306',
+       '0':u'\U00000030\U0000FE0F\U000020E3','1':u'\U00000031\U0000FE0F\U000020E3','2':u'\U00000032\U0000FE0F\U000020E3','3':u'\U00000033\U0000FE0F\U000020E3',
+       '4':u'\U00000034\U0000FE0F\U000020E3','5':u'\U00000035\U0000FE0F\U000020E3','6':u'\U00000036\U0000FE0F\U000020E3','7':u'\U00000037\U0000FE0F\U000020E3',
+       '8':u'\U00000038\U0000FE0F\U000020E3','9':u'\U00000039\U0000FE0F\U000020E3',
+       'fl':u'\U0001F4B8', 'smile':u'\U0000263A', 'rub':u'\U0001F6AF', 'warn':u'\U000026A0','fire':u'\U0001F525','dol':u'\U0001F4B2','peop':u'\U0001F465','monbag':u'\U0001F4B0',
+       'coup':u'\U0001F46B','link':u'\U0001F517','bor':u'\U00002639','redno':u'\U000026D4','card':u'\U0001F4B3','vosk':u'\U00002757','party':u'\U0001F389','paper':u'\U0001F4C3',
+       'rec':u'\U0001F9FE','dow':u'\U0001F447','gear':u'\U00002699','cht':u'\U0001F4AC','cas':u'\U0001F3B0','tri':u'\U0001F53B','coin':u'\U0001FA99','ua':u'\U0001F1FA\U0001F1E6',
+       'ke':u'\U0001F511','bas':u'\U0001F3C0','dice':u'\U0001F3B2','joy':u'\U0001F3AE','sm':u'\U0001F603','pain':u'\U0001F614','stat':u'\U0001F4CA','man':u'\U0001F464','vop':u'\U00002049','hand':u'\U0001F91D','inf':u'\U00002139','rekl':u'\U0001F48C',
+       'rass':u'\U0001F4AC','rekst':u'\U00002B50','redcr':u'\U0000274C','sh':u'\U0001F530','ana':u'\U0001F4C8'
+       }
